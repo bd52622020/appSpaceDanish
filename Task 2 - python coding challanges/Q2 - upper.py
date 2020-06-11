51ad3397f99bf511dc0cbeda577534009cbfacd7
DanishMahboob <mahboob.danish@appexperts.me.uk>
2020-06-11 14:56:04 +0100

    more fixing

---
+++ Task 2 - python coding challanges/Q2 - upper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def upperstring(string):
+    print(string.upper())
+        
+if __name__ == "__main__":
+    x = input("Please enter your string to be capitlised: ")
+    upperstring(x)
+
+
