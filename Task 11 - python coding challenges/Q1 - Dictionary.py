cf0ce3d15d543d09b75e3696e19942e9316f5762
DanishMahboob <mahboob.danish@appexperts.me.uk>
2020-06-15 13:11:48 +0100

    Update Q1 - Dictionary.py

--- Task 11 - python coding challenges/Q1 - Dictionary.py
+++ Task 11 - python coding challenges/Q1 - Dictionary.py
@@ -2,9 +2,7 @@
 
 dictu={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
-d = dict( (a,b) for a, b in dictu.items() if b < 5000)
-
-d = {k.upper(): v for k, v in d.items()}
+d = [k.upper() for (k,v) in dictu.items() if v < 5000]
 
 if __name__ == "__main__":
     print(d)
